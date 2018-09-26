f9276bc0bd95ed81f9e728483de1cb6a952cd3e4
greg <varankov@list.ru>
2018-09-26 19:13:34 +0300

    the roots of the quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const str = equation.split(" ");
+  const a = Number(str[0]);
+  const b = Number(str[3]+str[4]);
+  const c = Number(str[7]+str[8]);
+  const d = Math.pow(b , 2) - (4*a*c);
+  const x1 = Math.round((-b + Math.sqrt(d))/(2*a));
+  const x2 = Math.round((-b - Math.sqrt(d))/(2*a));
+  const arr = [x1,x2];
+  arr.sort(function(a, b) {
+    return a - b;
+  })
+  return arr;
 }
